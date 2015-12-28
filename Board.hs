e6205f979bc5f60127beb77675944c264448db82
Romain Gehrig <romain.gehrig@gmail.com>
2015-12-29 00:16:05 +0100

    Change Coordinate from (Char,Int) to (Int,Int)
    Simplifies functions on Coordinate

--- Board.hs
+++ Board.hs
@@ -3,6 +3,7 @@ module Board where
 
 import qualified Data.Map as M
 
+import Data.List
 import Data.Bits (xor)
 import Control.Lens
 
@@ -18,8 +19,8 @@ data Color = Black | White deriving (Eq, Show)
 data PieceType = Pawn | Bishop | King | Queen | Rook | Knight deriving (Eq, Ord, Show)
 data Piece = Piece { _color :: Color
                    , _pieceType :: PieceType } deriving (Eq)
-data Coordinate x y = Coordinate { _x :: x, _y :: y } deriving (Show, Eq, Ord)
-data Board = Board { _pieces :: M.Map (Coordinate Char Int) Piece } deriving (Eq)
+data Coordinate a = Coordinate { _x :: a, _y :: a } deriving (Show, Eq, Ord)
+data Board = Board { _pieces :: M.Map (Coordinate Int) Piece } deriving (Eq)
 
 data BackgroundColor = Light | Dark deriving (Show, Eq)
 backgroundColor :: BackgroundColor
@@ -36,22 +37,22 @@ makeLenses ''Piece
 makeLenses ''Coordinate
 makeLenses ''Board
 
-boardCoordinates :: [Coordinate Char Int]
-boardCoordinates = [Coordinate x y | y <- [8,7..1], x <- ['a'..'h']]
+boardCoordinates :: [Coordinate Int]
+boardCoordinates = [Coordinate x y | y <- [1..8], x <- [1..8]]
 
 instance Show Board where
   show (Board b) = foldr (\c r -> (maybe " " show (c `M.lookup` b)) ++
-                                  ( if view x c == 'h' && view y c /= 1 then "\n" else "") ++
+                                  (if view x c == 8 && view y c /= 1 then "\n" else "") ++
                                   r)
-                   "" boardCoordinates
+                   "" $ sortOn (\e -> (negate $ e ^. y, e ^. x)) boardCoordinates
 
-pawnLine :: Int -> Color -> [(Coordinate Char Int, Piece)]
-pawnLine l c = map (\r -> (Coordinate r l, Piece c Pawn)) ['a'..'h']
+pawnLine :: Int -> Color -> [(Coordinate Int, Piece)]
+pawnLine l c = map (\r -> (Coordinate r l, Piece c Pawn)) [1..8]
 
-piecesLine :: Int -> Color -> [(Coordinate Char Int, Piece)]
+piecesLine :: Int -> Color -> [(Coordinate Int, Piece)]
 piecesLine l c = zipWith (\t r -> (Coordinate r l, Piece c t))
                  [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
-                 ['a'..'h']
+                 [1..8]
 
 initBoard :: Board
 initBoard = Board ( M.fromList $
